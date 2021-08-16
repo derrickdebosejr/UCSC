9e8520389e58997910fb20ab58a38359d28ef6aa
derrickdebosejr <derrickdebosejr@gmail.com>
2021-08-16 16:46:21 -0700

    Add files via upload

---
+++ CMPS101S18PA1/aprat.java
@@ -0,0 +1,8 @@
+public class aprat extends apint{
+    apint numerator = new apint();
+    apint denominator = new apint();
+    aprat(apint numerator, apint denominator){
+        this.numerator=numerator;
+        this.denominator=denominator;
+    }
+}
