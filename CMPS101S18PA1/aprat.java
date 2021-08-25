b6aac362a089f218f267fbcce7045db08e01966f
derrickdebosejr <derrickdebosejr@gmail.com>
2021-08-25 14:46:59 -0700

    Delete CMPS101S18PA1 directory

--- CMPS101S18PA1/aprat.java
+++
@@ -1,8 +0,0 @@
-public class aprat extends apint{
-    apint numerator = new apint();
-    apint denominator = new apint();
-    aprat(apint numerator, apint denominator){
-        this.numerator=numerator;
-        this.denominator=denominator;
-    }
-}
